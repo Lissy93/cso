420f79181737a0125bc401e125fc5b0cd09150e0
Alicia Sykes <alicia@omg.lol>
2023-11-25 05:30:25 +0000

    Adds SCSS support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { createSignal } from 'solid-js'
 import solidLogo from './assets/solid.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
   const [count, setCount] = createSignal(0)
