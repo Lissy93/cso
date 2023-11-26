f159c0260aee53ee93b3ae0825c4ef5b1c7d40f3
Alicia Sykes <alicia@omg.lol>
2023-11-26 23:57:08 +0000

    Makes some navbar changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Component, createSignal } from 'solid-js';
 import { isAuthenticated, login, logout } from '../../services/authService';
+import Donut from '../../assets/donut.svg';
 import './Navbar.scss';
 
 const Navbar: Component = () => {
@@ -7,8 +8,11 @@ const Navbar: Component = () => {
   return (
     <nav class="navbar">
       <div class="navbar-brand">
-        <a href="/">Snack Champion</a>
-        </div>
+        <a href="/">
+          <img src={Donut} alt="Snack Champion" />
+          Snack Champion
+        </a>
+      </div>
       <ul class="navbar-links">
         <li><a href="/about">About</a></li>
         <li><a href="https://github.com/lissy93/cso" target="_blank">Source Code</a></li>
