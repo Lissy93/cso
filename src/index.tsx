df116956662a30b92403d8a2e9cc8aec204fce43
Alicia Sykes <alicia@omg.lol>
2023-11-26 23:57:30 +0000

    Resolve TS error when mounting possibly undefined DOM elem

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import { render } from 'solid-js/web';
 import AppRoutes from './Routes';
 
-import './styles/globals.scss'
-import './styles/typography.scss'
-import './styles/colors.scss'
+import './styles/globals.scss';
+import './styles/typography.scss';
+import './styles/colors.scss';
 
 render(
-  () => (
-    <AppRoutes />
-  ),
-  document.getElementById('snack-champion-root')
+  () => (<AppRoutes />),
+  document.getElementById('snack-champion-root')!
 );
