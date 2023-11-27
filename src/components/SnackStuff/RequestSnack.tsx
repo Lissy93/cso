586cfd2439cbb5e44cf9a15afc0eff11fb6c6b17
Alicia Sykes <alicia@omg.lol>
2023-11-27 22:21:20 +0000

    MVP requesting snacks functionality complete

--- src/components/SnackStuff/RequestSnack.tsx
+++ src/components/SnackStuff/RequestSnack.tsx
@@ -1,10 +1,12 @@
-import { Component, createSignal, Show } from 'solid-js';
+import { Component, createSignal, createEffect, onCleanup, Show } from 'solid-js';
 import { styled } from 'solid-styled-components';
 import { Motion, Presence } from '@motionone/solid'
 
 import supabase from '../../services/supabaseClient'
+import { fetchUserFromSession } from '../../services/authService'
 import Card from '../atoms/Card';
 import Button from '../atoms/Button';
+import SnackList from './SnackList';
 
 const SnackForm = styled('div')`
   display: flex;
@@ -12,6 +14,7 @@ const SnackForm = styled('div')`
   gap: 1rem;
   align-items: center;
   transition: all 0.2s ease-in-out;
+  margin-bottom: 2rem;
   @media (max-width: 768px) {
     flex-direction: column;
     align-items: inherit;
@@ -28,7 +31,6 @@ const SnackInputField = styled('input')`
   transition: all 0.2s ease-in-out;
   font-family: Poppins, sans-serif;
   flex-grow: 1;
-  width: 100%;
   &:focus {
     border: 1px solid var(--primary-darker);
   }
@@ -38,8 +40,63 @@ const SubHeading = styled('h2')`
   margin: 0 0 1rem;
 `;
 
+const fetchUserSnacks = async () => {
+  
+  const userId = (await fetchUserFromSession())?.id;
+  if (userId) {
+    const { data, error } = await supabase
+      .from('Snacks')
+      .select('*')
+      .eq('user_id', userId);
+
+    if (error) {
+      console.error('Error fetching snacks:', error);
+      return [];
+    }
+    return data;
+  }
+  return [];
+};
+
+interface SnackType {
+  snack_name: string;
+  user_id: string;
+}
+
 const RequestSnack: Component = () => {
   const [inputValue, setInputValue] = createSignal('');
+  const [userSnacks, setUserSnacks] = createSignal<SnackType[]>([]);
+  const [userId, setUserId] = createSignal<string | null>(null);
+
+
+  createEffect(async () => {
+    const session = await supabase.auth.getSession();
+    if (session.data && session.data.session && session.data.session.user) {
+      setUserId(session.data.session.user.id);
+      setUserSnacks(await fetchUserSnacks());
+    } else {
+      console.error('No active session found');
+    }
+  });
+  
+
+  // Set up a channel for real-time updates
+  const snackChannel = supabase
+    .channel('snack_updates')
+    .on(
+      'postgres_changes',
+      { event: 'INSERT', schema: 'public', table: 'Snacks' },
+      (payload) => {
+        if (payload.new.user_id === userId()) {
+          setUserSnacks([...userSnacks(), payload.new as SnackType]);
+        }
+      }
+    )
+    .subscribe();
+
+  onCleanup(() => {
+    supabase.removeChannel(snackChannel);
+  });
 
   const handleInputChange = (event: Event) => {
     const target = event.target as HTMLInputElement;
@@ -51,8 +108,7 @@ const RequestSnack: Component = () => {
     if (snackName) {
       // Get the currently logged-in user's ID
       const session = await (await supabase.auth.getSession()).data.session
-      console.log('Sesh', session);
-      const userId = session?.user?.id
+      const userId = session?.user?.id;
   
       // Ensure the user is logged in
       if (userId) {
@@ -62,7 +118,7 @@ const RequestSnack: Component = () => {
           .insert([{ snack_name: snackName, user_id: userId }]);
   
         if (error) {
-          console.error('Error inserting snack:', error);
+          console.error('Error inserting snack:', error.message);
         } else {
           console.log('Snack added:', data);
           setInputValue(''); // Clear the input field after submission
@@ -72,7 +128,6 @@ const RequestSnack: Component = () => {
       }
     }
   };
-  
 
   return (
     <Card>
@@ -87,7 +142,7 @@ const RequestSnack: Component = () => {
         <Presence>
         <Show when={inputValue().length > 0}>
           <Motion.div
-            animate={{ opacity: 1, width: '100px', overflow: 'visible' }}
+            animate={{ opacity: 1, width: 'auto', overflow: 'visible' }}
             transition={{ duration: 0.2,  ease: 'ease-in-out' }}
             initial={{ opacity: 0, width: 0, overflow: 'hidden' }}
             exit={{ opacity: 0, width: 0, overflow: 'hidden' }}
@@ -96,8 +151,10 @@ const RequestSnack: Component = () => {
           </Motion.div>
         </Show>
         </Presence>
-        
       </SnackForm>
+
+      <SubHeading>Your Requests</SubHeading>
+      <SnackList snacks={userSnacks} />      
     </Card>
   );
 };
---
+++ src/components/SnackStuff/SnackList.tsx
@@ -0,0 +1,46 @@
+import { Component, For } from 'solid-js';
+import { styled } from 'solid-styled-components';
+
+const SnackListWrap = styled('ul')`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  gap: 0.5rem;
+  li {
+    background: var(--background);
+    // padding: 0.5rem 0.75rem;
+    border-radius: 4px;
+    text-transform: capitalize;
+    box-shadow: 1px 1px 1px #000;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    img {
+      width: 3rem;
+      border-radius: 4px;
+    }
+  }
+`;
+
+interface SnackListProps {
+  snacks: () => any[]; // Accepts a function returning an array
+}
+
+const SnackList: Component<SnackListProps> = (props) => {
+  return (
+    <SnackListWrap>
+      <For each={props.snacks()}>
+        {(snack) => (
+          <li>
+            <img src={`https://snack-product-photo.as93.workers.dev/${snack.snack_name}/96`} alt="Snack" />
+            {snack.snack_name}
+          </li>
+        )}
+      </For>
+    </SnackListWrap>
+  );
+};
+
+export default SnackList;
