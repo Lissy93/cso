eeaffbd4609f238e4ec195f8bbc7d89993c39f20
Alicia Sykes <alicia@omg.lol>
2023-11-28 23:53:32 +0000

    Fixed the illegal top-level async in router err

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,52 +1,75 @@
-import { JSX, createEffect, onCleanup } from 'solid-js';
-import { Subscription } from '@supabase/gotrue-js';
+import { JSX, createEffect, createSignal, Show } from 'solid-js';
+import { ThemeProvider, createTheme } from '@suid/material/styles';
+import { Toaster } from 'solid-toast';
 
-import { Router, Routes, Route, useNavigate } from '@solidjs/router';
+import { Router, Routes, Route, Navigate } from '@solidjs/router';
 import LoginPage from './pages/LogIn';
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
+import ProfilePage from './pages/Profile';
+import RequestPage from './pages/Request';
+import VotesPage from './pages/Votes';
 import NotFoundPage from './pages/NotFound';
+
 import Navbar from './components/Navbar/Navbar';
 import { isAuthenticated } from './services/authService';
-
-// I don't know why this is required. But if I delete it, everything breaks.
 import supabase from './services/supabaseClient';
-await supabase.auth.getSession()
+
+const customTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#d82036',
+    },
+    secondary: {
+      main: '#fff',
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#b71c2b',
+      disabled: '#969798',
+    },
+    background: {
+      paper: '#161719',
+    },
+    mode: 'dark',
+  },
+});
 
 const ProtectedRoute = (props: { component: JSX.Element; }) => {
-  const navigate = useNavigate();
-  let effectCleanup: { data: { subscription: Subscription; }; } | (() => void);
 
-  createEffect(() => {
-    console.log(isAuthenticated());
-    if (!isAuthenticated()) {
-      navigate('/login');
-    } else {
-      effectCleanup = supabase.auth.onAuthStateChange((_event, session) => {
-        if (!session) {
-          navigate('/login');
-        }
-      });
-    }
-  }, isAuthenticated());
+  const [authCheckComplete, setAuthCheckComplete] = createSignal(false);
 
-  onCleanup(() => {
-    if (effectCleanup && typeof effectCleanup === 'function') effectCleanup();
+  createEffect(async () => {
+    await supabase.auth.getSession();
+    setAuthCheckComplete(true);
   });
 
-  return isAuthenticated() ? props.component : <div>Loading...</div>;
+  return (
+    <Show when={authCheckComplete()} fallback={<div>Loading authentication...</div>}>
+      {isAuthenticated() ? props.component : <Navigate href="/login" />}
+    </Show>
+  );
 };
 
 const AppRoutes = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/" element={<ProtectedRoute component={<HomePage />} />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <ThemeProvider theme={customTheme}>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<ProtectedRoute component={<HomePage />} />} />
+          <Route path="/profile" element={<ProtectedRoute component={<ProfilePage />} />} />
+          <Route path="/request" element={<ProtectedRoute component={<RequestPage />} />} />
+          <Route path="/vote" element={<ProtectedRoute component={<VotesPage />} />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Toaster
+          position="bottom-right"
+          toastOptions={{ style: { background: 'var(--background-lighter)', color: 'var(--foreground)',}, }}
+        />
+      </ThemeProvider>
     </Router>
   );
 };
