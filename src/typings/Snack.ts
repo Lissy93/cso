831f32d94ef3fa229b3ae478cc4ce26fe6a5b9e2
Alicia Sykes <alicia@omg.lol>
2023-11-28 23:54:15 +0000

    Defined some TypeScript BS for snacks

---
+++ src/typings/Snack.ts
@@ -0,0 +1,25 @@
+
+export type SnackCategory = 'sweet' | 'savory' | 'healthy' | 'drink';
+
+export interface Snack {
+  snack_id: string;
+  snack_name: string;
+  user_id: string;
+  created_at: string;
+  snack_category: SnackCategory;
+  snack_meta: string;
+}
+
+export type SnackVote = 'up' | 'down';
+
+export interface Vote {
+  vote_id: string;
+  vote: SnackVote;
+  snack_id: string;
+  user_id: string;
+  created_at: string;
+}
+
+export interface SnackWithVotes extends Snack {
+  Votes: Vote[];
+}
